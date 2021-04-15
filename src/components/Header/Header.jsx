d6f6d56eb33af947a41db581219e74c24e01caa2
unknown <sidorenko.irina.d@gmail.com>
2021-04-15 17:38:41 +0300

    file structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HeaderContainer = () => {
+  return <div>Заголовок</div>
+}
+
+export default HeaderContainer;
---
+++ src/components/InitialPage/InitialPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InitialPageContainer = () => {
+  return <div>Привет</div>
+}
+
+export default InitialPageContainer;
